3d37686cdcca5029ebda0b72af6ea4a7f5863efc
borashek <borashek@inbox.ru>
2022-11-05 09:29:51 +0300

    first task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from "react";
+import Header from "./components/header/Header"
+import Technologies from "./components/technologies/Technologies"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+      <div className="App">
+          <Header/>
+          <Technologies />
+      </div>
   );
 }
 

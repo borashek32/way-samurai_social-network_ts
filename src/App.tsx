9d00852b82857264428699a0e32b865a6a26b74b
borashek <borashek@inbox.ru>
2022-11-05 15:13:12 +0300

    sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Header from "./components/header/Header"
 import Technologies from "./components/technologies/Technologies"
-import './App.css';
+import './App.sass';
 
 function App() {
   return (
